68430e68c10bb6b8917d1b8343104a309c9c53d0
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-11-23 12:28:57 +0100

    fix: update to Node.js 22
    See https://aws.amazon.com/de/blogs/compute/node-js-22-runtime-now-available-in-aws-lambda/

--- cdk/BackendStack.ts
+++ cdk/BackendStack.ts
@@ -47,7 +47,7 @@ export class BackendStack extends Stack {
 				hash: layer.hash,
 			}).code,
 			compatibleArchitectures: [Lambda.Architecture.ARM_64],
-			compatibleRuntimes: [Lambda.Runtime.NODEJS_20_X],
+			compatibleRuntimes: [Lambda.Runtime.NODEJS_22_X],
 		})
 
 		const lambdaLayers: Lambda.ILayerVersion[] = [baseLayer]
